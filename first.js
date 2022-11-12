599291fda7143a3b7bed69e110d25f01db7ae63f
OksanaTarun <oxana.tarun@gmail.com>
2022-11-12 14:34:39 +0300

    add 9.2

---
+++ first.js
@@ -0,0 +1,9 @@
+const calculateBonus = (a, b) => {
+    let bonus;
+    const sum = a + b;
+    debugger; //при запуске дебаггера без указания точек остановки - останавливается на команде debugger, видим присвоенные значения переменным, посчитанную сумму
+    sum > 50 ? (bonus = 50) : (bonus = sum);
+    debugger; //следующая точка остановки, посчитан бонус согласно условию
+    return bonus;
+    };
+    console.log(calculateBonus(20,35))
